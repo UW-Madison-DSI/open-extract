b94959d7dca5256b1f8dc16c2ae4d46baf6ad16b
Jason Lo <lcmjlo@gmail.com>
2025-01-24 19:00:56 +0000

    add ArticleV2 model

--- open_extract/data_model.py
+++ open_extract/data_model.py
@@ -32,3 +32,23 @@ class Article(BaseModel):
             path = Path(f"data/jsons/{self.title}.json")
         with open(path, "w") as f:
             f.write(self.model_dump_json(indent=4))
+
+
+class ArticleV2(BaseModel):
+    title: str
+    authors: list[str]
+    publication_date: str
+    publication_name: str
+    publication_doi: str
+    methodology_summary: str
+    key_findings: str
+    insights_on_the_effect_of_plant_date_on_soybean_yield: str
+    study_independent_variables: list[str]
+    study_dependent_variables: list[str]
+    data_points: list[DataPoint]
+
+    def save(self, path: Path | None = None):
+        if not path:
+            path = Path(f"data/jsons/{self.title}.json")
+        with open(path, "w") as f:
+            f.write(self.model_dump_json(indent=4))
