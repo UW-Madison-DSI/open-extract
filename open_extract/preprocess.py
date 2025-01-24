131f39e81a74c1a224efd797a87cab8e31e14054
Jason Lo <lcmjlo@gmail.com>
2025-01-24 19:00:48 +0000

    add pdf2jpg function

--- open_extract/preprocess.py
+++ open_extract/preprocess.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pymupdf
 
 from llama_parse import LlamaParse, ResultType
 
@@ -13,3 +14,13 @@ def pdf2md(pdf: Path, save_path: Path) -> str:
     with open(save_path, "w") as f:
         f.write(md)
     return md
+
+
+def pdf2jpg(pdf_path: Path, save_path: Path) -> None:
+    """Convert PDF into multiple JPG images."""
+
+    save_path.mkdir(parents=True, exist_ok=True)
+    with pymupdf.open(pdf_path) as pdf:
+        for page in pdf:
+            pix = page.get_pixmap()
+            pix.save(save_path / f"{page.number}.jpg")
