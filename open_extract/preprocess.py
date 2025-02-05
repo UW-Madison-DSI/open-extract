fd7efb0f4430fd4cad9ffbe20a6e644564d0bf38
Jason Lo <lcmjlo@gmail.com>
2025-02-05 21:03:43 +0000

    test new proprocessor and preprocess new batch

--- open_extract/preprocess.py
+++ open_extract/preprocess.py
@@ -1,18 +1,45 @@
 from pathlib import Path
 import pymupdf
-
+import openparse
+import pymupdf4llm
 from llama_parse import LlamaParse, ResultType
+from markitdown import MarkItDown
 
 
-def pdf2md(pdf: Path, save_path: Path) -> str:
-    """Convert a PDF to a markdown string."""  # TODO: use non-proprietary parser
+def pdf2md_llamaparse(pdf: Path, save_path: Path) -> str:
+    """Convert a PDF to a markdown string. (External API, not recommended)"""
 
     parser = LlamaParse(result_type=ResultType.MD)
     document = parser.load_data(str(pdf))
 
     md = "\n".join([doc.text for doc in document])
-    with open(save_path, "w") as f:
-        f.write(md)
+    save_path.write_text(md)
+    return md
+
+
+def pdf2md_markitdown(pdf: Path, save_path: Path) -> str:
+    """Use Markitdown to convert a PDF to a markdown string. (Poor quality, not recommended)"""
+
+    result = MarkItDown().convert(str(pdf))
+    md = result.text_content
+    save_path.write_text(md)
+    return md
+
+
+def pdf2txt(pdf_path: Path, save_path: Path) -> str:
+    """Convert a PDF to a text string. (Interesting nodes output, explore later.)"""
+
+    parsed_doc = openparse.DocumentParser().parse(pdf_path)
+    text = "\n\n".join([node.text for node in parsed_doc.nodes])
+    save_path.write_text(text)
+    return text
+
+
+def pdf2md(pdf: Path, save_path: Path) -> str:
+    """Convert a PDF to a markdown string with pymupdf."""
+
+    md = pymupdf4llm.to_markdown(pdf)
+    save_path.write_text(md)
     return md
 
 
